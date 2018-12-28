b21c14745cc8b7dead5dc6b5fc24c7af888b8360
Domingo Cabeza <domingoacd1@gmail.com>
2018-12-28 19:51:03 -0400

    Added classes

---
+++ book.js
@@ -0,0 +1,8 @@
+class Book{
+    constructor(name, author, code, status){
+        this.name = name;
+        this.author = author;
+        this.code = code;
+        this.status = status;
+    }
+}
